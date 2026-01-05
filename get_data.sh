e09b3a3ec6165ab97dc27456c3ddb5778ac95dc8
samkc-0 <sam.k.c.0@icloud.com>
2026-01-05 14:41:48 -0600

    init

---
+++ get_data.sh
@@ -0,0 +1,3 @@
+url=https://crtm.maps.arcgis.com/sharing/rest/content/items/5c7f2951962540d69ffe8f640d94c246/data
+curl $url -o data.zip
+unzip data.zip -d data
---
+++ go.mod
@@ -0,0 +1,3 @@
+module madrid
+
+go 1.25.1
